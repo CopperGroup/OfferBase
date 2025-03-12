6ec07d5eee29a02b388bb8261a3c030dd2e1c106
tymyevhenmpw <tymyevhen@gmail.com>
2025-03-13 00:26:03 +0100

    Initial commit

---
+++ constants/store.js
@@ -0,0 +1,12 @@
+export const Store = {
+  "name": "OfferBase",
+  "currency": "UAH",
+  "currency_sign": "₴",
+  "domain": "https://fo-scandinavia.vercel.app",
+  "database": "OfferBase_2025_03_13",
+  "default_image": {
+    "product_card": 0,
+    "product_page": 0,
+    "checkout_page": 0
+  }
+};
